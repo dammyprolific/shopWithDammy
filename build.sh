ae20a74df28fdac7a7077ed6a2bf50bc64af1f07
Adeyemi Damilare Tomisin <142660624+dammyprolific@users.noreply.github.com>
2025-08-30 11:52:56 -0700

    Initial commit2

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-set -o errexit # exit on error
+# Exit on error
+set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Run migrations
 python manage.py migrate
-python manage.py collectstatic --no-input
+
+# Collect static files
+python manage.py collectstatic --noinput
