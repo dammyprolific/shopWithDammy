698b1c9001b4e3f76499b0f76322b0a6171b70b6
Adeyemi Damilare Tomisin <142660624+dammyprolific@users.noreply.github.com>
2025-10-02 08:55:16 -0700

    neeW1

--- ShopWithDammy/settings.py
+++ ShopWithDammy/settings.py
@@ -11,7 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+
+ALLOWED_HOSTS = [
+    "shopwithdammy2.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
