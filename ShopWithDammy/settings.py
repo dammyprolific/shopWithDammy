17cddf49cc41b9809e3bed6a5819f73a5f5fb239
Adeyemi Damilare Tomisin <142660624+dammyprolific@users.noreply.github.com>
2025-10-02 06:46:32 -0700

    errors

--- ShopWithDammy/settings.py
+++ ShopWithDammy/settings.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'tw5^_afz6ck%p3g61nts3mo#jq85_*z1l-tmhqb$j7mi6j^%y!@#n'
-DEBUG = False
+SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ["shopwithdammy2.onrender.com", "localhost", "127.0.0.1"]
 
@@ -103,11 +103,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
 }
 
-FLUTTERWAVE_SECRET_KEY = 'FLWSECK_TEST-5178b0f76422f10d30d9457cb284a344-X'
+FLUTTERWAVE_SECRET_KEY = os.getenv("FLUTTERWAVE_SECRET_KEY", "'FLWSECK_TEST-5178b0f76422f10d30d9457cb284a344-X")
 
-PAYPAL_CLIENT_ID = 'ARWe8mgWX6U5lYZOA2_eCEdCqn626MAykueQ0Chug-VqNDgJiIMlS5QrBRvr_Hh1R9KDlHpAeU0d3aC0'
-PAYPAL_SECRET_KEY = 'EBrLuxHP_VZUz7tMGKtDDprzV1yps3wIHXQzXwS_PAaOr77_CYFxZWv50fmmbnpWFaKdDnEBcX3WOWY5'
-PAYPAL_MODE = 'sandbox'  # Change to 'live' for production
+PAYPAL_CLIENT_ID = os.getenv("PAYPAL_CLIENT_ID", "ARWe8mgWX6U5lYZOA2_eCEdCqn626MAykueQ0Chug-VqNDgJiIMlS5QrBRvr_Hh1R9KDlHpAeU0d3aC0")
+PAYPAL_SECRET_KEY = os.getenv("PAYPAL_SECRET_KEY", "EBrLuxHP_VZUz7tMGKtDDprzV1yps3wIHXQzXwS_PAaOr77_CYFxZWv50fmmbnpWFaKdDnEBcX3WOWY5")
+PAYPAL_MODE = os.getenv("PAYPAL_MODE", "sandbox")  # Change to 'live' for production
 
 REACT_BASE_URL = os.getenv("REACT_BASE_URL", "http://localhost:5173")
 
