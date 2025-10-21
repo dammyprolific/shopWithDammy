08989747d7034bce32f4da3ff82248bb0dd5627b
Adeyemi Damilare Tomisin <142660624+dammyprolific@users.noreply.github.com>
2025-10-21 05:37:39 -0700

    new commit

--- Shopping_App/models.py
+++ Shopping_App/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.conf import settings
+from cloudinary.models import CloudinaryField
 
 class Products(models.Model):
     CATEGORY = [
@@ -15,7 +16,7 @@ class Products(models.Model):
 
     name = models.CharField(max_length=100)
     slug = models.SlugField(blank=True, null=True)
-    image = models.ImageField(upload_to="products/")
+    image = CloudinaryField('image', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=15, choices=CATEGORY, blank=True, null=True)
